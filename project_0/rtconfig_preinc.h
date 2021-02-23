36876ef73aaa6e7b373b044c3e9c879fceddb07d
zouyu <zouyu@rt-thread.com>
2021-02-23 09:55:24 +0800

    first

---
+++ project_0/rtconfig_preinc.h
@@ -0,0 +1,14 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define CORE_CM7
+#define HAVE_CCONFIG_H
+#define STM32H747xx
+#define USE_HAL_DRIVER
+#define __RTTHREAD__
+
+#endif /*RTCONFIG_PREINC_H__*/
